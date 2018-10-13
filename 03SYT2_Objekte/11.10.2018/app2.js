b38b6fde2b1dde06499760a03e23de9676406039
NoahStallinger <43745548+NoahStallinger@users.noreply.github.com>
2018-10-13 14:05:25 +0200

    11.10.2018_03SYT2web_Objekte

---
+++ 03SYT2_Objekte/11.10.2018/app2.js
@@ -0,0 +1,11 @@
+/*
+let name = 'Susi';
+console.log('name = ', name, name.constructor);*/
+
+let hallo = function (str) {
+    str= "Hallo " + str;
+};
+
+let welt = "Welt";
+hallo(welt);
+console.log(welt);
